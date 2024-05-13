b3ba80bf24ed219b98a0a4b594c4f745839335bc
Biomathematicus <biomathematicus@gmail.com>
2024-05-12 23:30:05 -0500

    Initial deployment

---
+++ assistant.py
@@ -0,0 +1,36 @@
+from openai import OpenAI
+client = OpenAI()
+
+# Create an Assistant  
+assistant = client.beta.assistants.create(
+  name="Math Tutor",
+  instructions="You are a personal math tutor. Write and run code to answer math questions.",
+  tools=[{"type": "code_interpreter"}],
+  model="gpt-4-turbo",
+)
+
+# Create a Thread
+thread = client.beta.threads.create()
+
+# Add a Message to the Thread
+message = client.beta.threads.messages.create(
+  thread_id=thread.id,
+  role="user",
+  content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+)
+
+# Create a Run Without Streaming
+run = client.beta.threads.runs.create_and_poll(
+  thread_id=thread.id,
+  assistant_id=assistant.id,
+  instructions="Please address the user as Jane Doe. The user has a premium account."
+)
+
+# Once the Run completes, you can list the Messages added to the Thread by the Assistant.
+if run.status == 'completed': 
+  messages = client.beta.threads.messages.list(
+    thread_id=thread.id
+  )
+  print(messages)
+else:
+  print(run.status)
